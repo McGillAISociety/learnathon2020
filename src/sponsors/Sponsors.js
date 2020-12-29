0b652d26819c6c063b64cadf2381714b46f96fe1
Raphaelle Tseng <33766912+raphaelletseng@users.noreply.github.com>
2020-12-29 12:06:28 -0500

    Add Sponsors.js

---
+++ src/sponsors/Sponsors.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import styled from '@emotion/styled';
+import Title from '../Title.js';
+
+class Sponsors extends Component {
+  render(){
+    return (
+      <section id = "sponsors">
+        <Title titleColor = "#8becff"> Sponsors </Title>
+      </section>
+    );
+  }
+}
+export default Sponsors;
