2c3b8d4c7b268dc1e525ca74d963bbff9cc4d2fe
Raphaelle Tseng <33766912+raphaelletseng@users.noreply.github.com>
2020-12-29 12:02:56 -0500

    Update FAQ.js

--- src/faq/FAQ.js
+++ src/faq/FAQ.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import styled from '@emotion/styled';
+import {Accordion, AccordionItem} from 'react-light-accordion';
 
 //import './schedule.css';
 import Title from '../Title.js';
@@ -7,9 +9,36 @@ class FAQ extends Component{
   render(){
     return(
       <section id = "faq">
+      <div className="faq-container">
       <div>
-        <Title titleColor = "#2b2b2b">FAQ</Title>
+        <Title titleColor = "#ffa9a8">FAQ</Title>
       </div>
+      <div>
+      <Accordion atomic ={true}>
+        <AccordionItem title = "Can I register for this conference?">
+        <p> Yes! Anyone can participate. The event if open to everyone
+        interested in learning more about AI. </p>
+        </AccordionItem>
+
+        <AccordionItem title = "I am new to AI. Can I still participate?">
+        <p> Yup! All skill levels are welcome. More importantly, we aim to provide
+        a condensed, fun, open, learning experience. </p>
+        </AccordionItem>
+
+        <AccordionItem title = "Does it cost anything to take part?">
+        <p> Nope! The event is completely free of charge. </p>
+        </AccordionItem>
+
+        <AccordionItem title = "Do I have to learn something in advance?">
+        <p> Some mathematical background is recommended but not necessary!
+        We will be having a talk presenting the important mathematical topics used in AI.</p>
+        </AccordionItem>
+
+
+      </Accordion>
+      </div>
+      </div>
+
       </section>
     );
   }
